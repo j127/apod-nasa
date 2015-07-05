ea2dc3c03aadd5376c140696f6d3a44143a9fb81
jesse <jessehall3@gmail.com>
2015-07-05 12:58:07 -0700

    add pic to detail template

--- app/apod/views/pictures.py
+++ app/apod/views/pictures.py
@@ -9,5 +9,7 @@ pictures_bp = Blueprint('pictures', __name__, url_prefix='/apod')
 @pictures_bp.route('/')
 def index():
 	pic_data = fetch_photo()
-
-	return jsonify(pic_data)
+	data = {}
+	data['title'] = pic_data['title']
+	data['img_url'] = pic_data['url']
+	return render_template('pictures/detail.html', data=data)
