5f6a03ebdedb5c5ff5e30b4c1d6e36fcfa7b7f3d
jesse <jessehall3@gmail.com>
2015-07-05 12:47:39 -0700

    pic request

--- app/apod/util/fetcher.py
+++ app/apod/util/fetcher.py
@@ -1,5 +1,9 @@
 import requests
-from .. import app
 
+def fetch_photo():
+	# api_key = app.config['NASA_API_KEY']
+	api_key = 'ldWyF16dCc1uyXUTWpRAC2O3QGOzZRuINM7Mjo08'
+	url = 'https://api.nasa.gov/planetary/apod?concept_tags=True&api_key={}'.format(api_key)
 
-api_key = app.config['NASA_API_KEY']
+	res = requests.get(url)
+	return res.text
--- app/apod/views/pictures.py
+++ app/apod/views/pictures.py
@@ -1,8 +1,15 @@
-from flask import Blueprint, render_template, url_for
+import json
+from flask import Blueprint, render_template, url_for, jsonify
 # from ..models.posts import Picture, Video
+
+from ..util.fetcher import fetch_photo
+
 
 pictures_bp = Blueprint('pictures', __name__, url_prefix='/apod')
 
 @pictures_bp.route('/')
 def index():
-	return 'hello from pictures'
+	pic = fetch_photo()
+	pic_json = json.loads(pic)
+
+	return jsonify(pic_json)
