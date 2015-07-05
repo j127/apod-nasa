8817210b5ccf43c88b461e0e6a4410233621a6c4
jesse <jessehall3@gmail.com>
2015-07-05 14:24:29 -0700

    add dynamic date requests

--- app/apod/views/pictures.py
+++ app/apod/views/pictures.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, url_for, jsonify, redirect
+from flask import Blueprint, render_template, url_for, jsonify, redirect, abort
 # from ..models.posts import Picture, Video
 
 from ..util.fetcher import fetch_photo
@@ -14,6 +14,19 @@ def redirect_to_current_day():
 @pictures_bp.route('/astropix.html')
 def pictures_index():
 	pic_data = fetch_photo()
+	data = {}
+	data['title'] = pic_data['title']
+	data['img_url'] = pic_data['url']
+	data['explanation'] = pic_data['explanation']
+	return render_template('pictures/detail.html', data=data)
+
+@pictures_bp.route('/ap<int:picture_date>.html')
+def picture_detail(picture_date):
+	date_str = str(picture_date)
+	if len(date_str) != 6:
+		return abort(404)
+	split_date = [date_str[i:i+2] for i in range(0, len(date_str), 2)]
+	pic_data = fetch_photo(split_date[0], split_date[1], split_date[2])
 	data = {}
 	data['title'] = pic_data['title']
 	data['img_url'] = pic_data['url']
