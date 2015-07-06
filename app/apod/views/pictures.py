1e58fc4f13c849f86f432a1a6fe1a06169b92a03
jesse <jessehall3@gmail.com>
2015-07-06 15:31:49 -0700

    add test for database

--- app/apod/views/pictures.py
+++ app/apod/views/pictures.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, url_for, jsonify, redirect, abort
-# from ..models.posts import Picture, Video
+from ..models.pictures import Picture
 
 from ..util.api_client.client import fetch_photo
 from ..util.helpers import pic2data
@@ -25,6 +25,16 @@ def pictures_index():
 
 @pictures_bp.route('/ap<picture_date>.html')
 def picture_detail(picture_date):
+    date_str = str(picture_date)
+    if len(date_str) != 6:
+        return abort(404)
+    split_date = [date_str[i:i + 2] for i in range(0, len(date_str), 2)]
+    pic_data = fetch_photo(split_date[0], split_date[1], split_date[2])
+    data = pic2data(pic_data)
+    return render_template('pictures/detail.html', data=data)
+
+@pictures_bp.route('/2/ap<picture_date>.html')
+def picture_detail2(picture_date):
     date_str = str(picture_date)
     if len(date_str) != 6:
         return abort(404)
